3c0f650de97e9cabb540aa3902ac007dbc7a3989
Vitaly Efremov <efremov.vitaly@gmail.com>
2018-03-30 20:15:11 +0700

    Enhanced the allowed hosts for local

--- app/settings/local.py
+++ app/settings/local.py
@@ -1,10 +1,8 @@
 from .base import *
 
-INTERNAL_IPS = ['127.0.0.1']
-ALLOWED_HOSTS += INTERNAL_IPS
-ALLOWED_HOSTS.append('localhost')
-# INSTALLED_APPS.append('debug_toolbar')
-# MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+ALLOWED_HOSTS = [
+    '*'
+]
 
 DEBUG = True
 
