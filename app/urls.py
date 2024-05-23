b6a227899544b6bc2a1a09a538cf75233b85bf94
v.efremov <v.efremov@cian.ru>
2024-05-23 09:54:17 +0700

    Upgrade to Django 4.1

--- app/urls.py
+++ app/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path(r'^admin/', admin.site.urls),
 ]
